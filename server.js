5bdcf003e4c7dd84c370cc104e425f1cf2cc1f36
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-09-23 16:34:04 -0400

    moved routes to routes directory

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const PORT = 8000
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.render('index.ejs')
-})
-
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Listening on localhost:${PORT}`)
 })
