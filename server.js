1fb1364a7060f24f2874f4cfb797ebbccc173be2
dcummings856 <68401373+dcummings856@users.noreply.github.com>
2021-09-24 07:48:05 -0400

    connected routes/index to server.js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
 const PORT = 8000
+const indexRoute = require('./routes/index')
+
+require('dotenv').config({path: './config/.env'})
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use('/', indexRoute)
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Listening on localhost:${PORT}`)
